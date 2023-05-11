69e0b3048d7772d04f8507292c715377f20ee6b5
Cole Foster <cole@thefostersonline.com>
2023-05-11 12:57:25 -0400

    removed test files

--- components/brainfoo/BrainFoo.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import BrainFoo from './BrainFoo'
-
-describe('<BrainFoo />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<BrainFoo />)
-  })
-})
